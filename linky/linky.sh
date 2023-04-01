5ece4580d2a557b4f9a20505f9565c74407fee96
Ajam <AjamX101@gmail.com>
2023-04-01 17:10:50 +0545

    .

--- linky/linky.sh
+++ linky/linky.sh
@@ -221,9 +221,9 @@ scope_domain=$(echo "$url" | unfurl apexes)
 #Extract full domain name
 domain=$(echo "$url" | unfurl domains)
 #Set .scope 
-echo ""
 echo "Scope is set as: "
 echo $scope_domain | scopegen -in | tee /tmp/$domain.scope
+echo ""
 #Start Tools
 #Gau
 echo "➼ Running gau on: $url" && sleep 3s
